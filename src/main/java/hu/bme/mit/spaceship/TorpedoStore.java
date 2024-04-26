4b2320bbf28394d53386afbe8dca16540c510d82
Szarkowicz Dániel <szarkowicz.dani@proton.me>
2024-04-26 12:53:22 +0200

    move generator creation to constructor

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -14,9 +14,10 @@ public class TorpedoStore {
 
   private int torpedoCount = 0;
 
-  private Random generator = new Random();
+  private Random generator;
 
   public TorpedoStore(int numberOfTorpedos){
+    this.generator = new Random();
     this.torpedoCount = numberOfTorpedos;
 
     // update failure rate if it was specified in an environment variable
